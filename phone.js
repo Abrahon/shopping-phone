24b09f9211da1abd22a2e2e7fa136a8ce11d7a89
Abrahon <90842196+Abrahon@users.noreply.github.com>
2022-08-20 11:04:15 +0600

    first commit

---
+++ phone.js
@@ -0,0 +1,47 @@
+function updatedPhoneNumber(isIncrease) {
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const phoneNumberString = phoneNumberField.value;
+    const previousPhoneNumber = parseInt(phoneNumberString);
+
+
+    let newPhoneNumber;
+
+    if (isIncrease) {
+        newPhoneNumber = previousPhoneNumber + 1;
+    }
+    else {
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
+
+    phoneNumberField.value = newPhoneNumber;
+    return newPhoneNumber;
+}
+function updatedPhoneTotalPrice(newPhoneNumber) {
+    const phoneTotalPrice = newPhoneNumber * 1219;
+
+    const phoneTotalElement = document.getElementById('phone-total');
+    phoneTotalElement.innerText = phoneTotalPrice;
+
+}
+
+document.getElementById('btn-phone-plus').addEventListener('click', function () {
+
+    const newPhoneNumber = updatedPhoneNumber(true);
+
+    updatedPhoneTotalPrice(newPhoneNumber);
+    calculateSubTotal();
+
+
+    //calculate total
+});
+
+
+document.getElementById('btn-phone-minus').addEventListener('click', function () {
+
+    const newPhoneNumber = updatedPhoneNumber(false);
+
+    updatedPhoneTotalPrice(newPhoneNumber);
+    calculateSubTotal();
+
+});
+
